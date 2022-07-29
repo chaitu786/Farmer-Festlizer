008b967b62573cdbd6b016c105d3dbe9a4984940
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2022-07-30 00:08:40 +0530

    chat

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
-import { store } from './Redux/Store';
-import {BrowserRouter} from "react-router-dom"
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-const breakpoints = {
-  sm: '400px',
-  md: '768px',
-  lg: '960px',
-  xl: '1200px',
-  '2xl': '1536px',
-}
-
-
-const theme = extendTheme({ breakpoints })
-
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <ChakraProvider Provider={theme}>
-        <App />
-      </ChakraProvider>
-    </Provider>
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
