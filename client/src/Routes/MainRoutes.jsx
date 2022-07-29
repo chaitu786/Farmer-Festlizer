deb8ebb4f9ccb6b5f234b35f9b45956d191bab51
Shravankumar1234 <99716701+Shravankumar1234@users.noreply.github.com>
2022-07-30 01:31:47 +0530

    final

--- client/src/Routes/MainRoutes.jsx
+++ client/src/Routes/MainRoutes.jsx
@@ -4,9 +4,13 @@ import { Login } from '../Components/Auth/Login'
 import { SignUp } from '../Components/Auth/Signup'
 import { FarmerPage } from '../Components/Home/FarmerPage'
 
+import NavBar from '../Components/Home/Header'
+
 const MainRoutes = () => {
   return (
     <div>
+      {/* <NavBar/> */}
+<NavBar/>
         <Routes>
             <Route path="/" element={<FarmerPage/>} ></Route>
             <Route path="/signup" element={<SignUp/>} ></Route>
