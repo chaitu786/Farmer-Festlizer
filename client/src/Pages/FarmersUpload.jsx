53b229c8f70191da0f8373797bff615c3f1b9ce5
chaitu786 <chaithanyareddy786@gmail.com>
2022-07-29 09:36:38 +0530

    connected_frontend_with_backend

--- client/src/Pages/FarmersUpload.jsx
+++ client/src/Pages/FarmersUpload.jsx
@@ -6,13 +6,11 @@ import { Get_data } from '../Redux/AppReducer/Action'
 import "./Farmers.css"
 
 export const FarmersUpload = () => {
-    const ProductData=useSelector((state)=>state.Reducer.Products);
+    const ProductData=useSelector((state)=>state.Reducer.Products?.data);
     const dispatch=useDispatch();
-
-
     useEffect(()=>{
         dispatch(Get_data())
-    },[])
+    },[dispatch])
 console.log(ProductData)
   return (
    <Center>
@@ -23,11 +21,11 @@ console.log(ProductData)
             <Stack direction={{base:"column",md:'row'}} border={'1px solid black'}
             width={{base:"sm",md:"md",lg:"lg",xl:"xl"}} mt={'10'}   borderRadius={"20px"} className='MainCartItem'>
             <Box padding={{base:"8",md:'8'}}>
-                <Image src={el.Image}  width={{base:"sm",md:"md",lg:"lg",xl:"xl"}}/>
+                <Image src={el.Image_Url}  width={{base:"sm",md:"md",lg:"lg",xl:"xl"}}/>
             </Box>
             <Box padding={{base:"8",md:'10'}}>
-                <Text>{el.Problem}</Text>
-                <Text>{el.location}</Text>
+                <Text>{el.Title}</Text>
+                <Text>{el.Desc}</Text>
                 <Box>
                     <ButtonCart/>
                 </Box>
--- client/src/Redux/AppReducer/Action.js
+++ client/src/Redux/AppReducer/Action.js
@@ -4,10 +4,19 @@ import axios from "axios"
 
 export const Get_data=()=>(dispatch)=>{
  dispatch({type:types.PRODUCT_REQUEST});
- axios.get("http://localhost:3000/Data").then((r)=>{
+ axios.get("http://localhost:8080/data").then((r)=>{
     dispatch({type:types.PRODUCT_SUCCESS,payload:r.data})
  }).catch((e)=>{
     dispatch({type:types.PRODUCT_FALIURE})
  })
-
 };
+
+export const Get_User=()=>(dispatch)=>{
+   axios.get("http://localhost:8080/users").then((r)=>{
+      console.log(r);
+      dispatch({type:types.USER_DATA,payload:r.data})
+   })
+   .catch((err)=>{
+      console.log(err);
+   })
+  };
