c5a923fd0fda3c37449062c6069e44cc5cc977f2
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2022-07-30 00:10:29 +0530

    chatapplicationdone

--- server/server.js
+++ server/server.js
@@ -1,55 +1,16 @@
-const express=require('express')
-const mongoose = require("mongoose");
-const cors=require("cors")
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv").config();
-const userRouter=require("./Routes/User.Routes");
-const session = require('express-session');
-const { productRouter } = require('./Routes/Product.Routes');
-const cartRouter = require('./Routes/Cart.Routes');
-const postsRouter = require('./Routes/MyPosts.Routes');
+const io = require('socket.io')(5000)
 
-const app=express()
+io.on('connection', socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
 
-app.set("trust proxy", 1); // trust first proxy
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    sameSite: "none",
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
   })
-);
-
-app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true
-}))
-
-app.use(cookieParser());
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.get('/',(req,res)=>{res.send('hello')})
-
-app.use("/",userRouter)
-app.use("/",productRouter)
-app.use("/",cartRouter)
-app.use("/",postsRouter)
-
-const PORT = process.env.PORT || 8080;
-const mongoDB = process.env.MongoAtlas;
-
-app.listen(PORT,()=> { 
-    mongoose
-    .connect(mongoDB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("connection successful to server");
-    })
-    .catch((err) => {
-      console.log(err, "Failed to connect to server");
-    });
-  console.log(`Listening on Port ${PORT}`);
 })
