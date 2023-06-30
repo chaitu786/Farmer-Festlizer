f7f1e2e35bd46c302dc0676c00b1404abade92e5
chaitu786 <chaithayareddy786@gmail.com>
2023-06-30 17:38:01 +0530

    cookie mey small change

--- server/Routes/User.Routes.js
+++ server/Routes/User.Routes.js
@@ -1,6 +1,7 @@
 const { Router }=require("express");
 const { SignUpUser,LoginUser, getAllUsers } = require("../Controller/User.controller");
 const userRouter = Router();
+const Cookies = require('cookies');
 
 // create user
 userRouter.post("/signup", async(req,res)=>{
@@ -33,6 +34,7 @@ userRouter.post("/signup", async(req,res)=>{
 //login
 userRouter.post("/login", async (req, res) => {
     const { Mobile, Password } = req.body;
+    const cookies = new Cookies(req, res);
     console.log(Mobile );
     if (Mobile && Mobile.toString().length !== 10) {
         return res.status(200).send({ message: "Invalid Mobile Number" });
@@ -49,7 +51,8 @@ userRouter.post("/login", async (req, res) => {
     } else if (status === "failed") {
       return res.status(201).send({ message, status });
     }
-    res.cookie("auth", value, { httpOnly: true, secure: true})
+    cookies.set("auth", value, { httpOnly: true, secure: true})
+    // res.cookie("auth", value, { httpOnly: true, secure: true})
     return res
       .status(200)
       .send({ message, status, value, data });
